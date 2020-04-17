95b50948d4bf0c68f0b60e7f2f2abf8a13a4a0fa
Martha Rettig <marthar@cykod.com>
2020-04-17 09:29:18 -0400

    Updates

---
+++ precache-manifest.d4335b7d776ea6943b830092b156ae32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7054c63e3afc2b0ab868debf568f8048",
+    "url": "/massart-portfolios/index.html"
+  },
+  {
+    "revision": "f11e7e3000258d009f26",
+    "url": "/massart-portfolios/static/css/main.70f39561.chunk.css"
+  },
+  {
+    "revision": "c5f20fe874268b39772c",
+    "url": "/massart-portfolios/static/js/2.f226cda7.chunk.js"
+  },
+  {
+    "revision": "d231813f6fb47aa56cf6f9aa1606f022",
+    "url": "/massart-portfolios/static/js/2.f226cda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11e7e3000258d009f26",
+    "url": "/massart-portfolios/static/js/main.5105147c.chunk.js"
+  },
+  {
+    "revision": "f84db9247f377f6403d9",
+    "url": "/massart-portfolios/static/js/runtime-main.03d420b9.js"
+  }
+]);
