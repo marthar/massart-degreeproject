1026442ee7b4fd0593644f450c6c99b03c14a276
Martha Rettig <marthar@cykod.com>
2020-04-20 19:49:22 -0400

    Updates

---
+++ precache-manifest.efcc0b36cde62d496f206d31e72c85eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa197ff160f3da25ecb17578388e8e8",
+    "url": "/massart-portfolios/index.html"
+  },
+  {
+    "revision": "b092afdfff40fdc3fedb",
+    "url": "/massart-portfolios/static/css/main.6ac2ec58.chunk.css"
+  },
+  {
+    "revision": "11f0e74e3a6b6d10ad5f",
+    "url": "/massart-portfolios/static/js/2.cda44798.chunk.js"
+  },
+  {
+    "revision": "d231813f6fb47aa56cf6f9aa1606f022",
+    "url": "/massart-portfolios/static/js/2.cda44798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b092afdfff40fdc3fedb",
+    "url": "/massart-portfolios/static/js/main.a2e8b3a9.chunk.js"
+  },
+  {
+    "revision": "f84db9247f377f6403d9",
+    "url": "/massart-portfolios/static/js/runtime-main.03d420b9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5586d129be29959830492e8c58868e30.js"
+  "/massart-portfolios/precache-manifest.efcc0b36cde62d496f206d31e72c85eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/massart-portfolios/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
